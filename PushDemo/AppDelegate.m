57d808946ac4abab44810c4c424690a4328bd492
berning <berningwang@icloud.com>
2021-02-28 21:56:32 +0800

    apns origin added

--- PushDemo/AppDelegate.m
+++ PushDemo/AppDelegate.m
@@ -6,8 +6,9 @@
 //
 
 #import "AppDelegate.h"
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
 
 @end
 
@@ -15,9 +16,69 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [self push];
     return YES;
 }
+
+- (void)push
+{
+    if (@available(iOS 10.0, *)) {
+        [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:UNAuthorizationOptionBadge|UNAuthorizationOptionSound|UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            NSLog(@"%@", error);
+
+        }];
+        
+        UNNotificationCategory* generalCategory = [UNNotificationCategory
+                                                   categoryWithIdentifier:@"GENERAL"
+                                                   actions:@[]
+                                                   intentIdentifiers:@[]
+                                                   options:UNNotificationCategoryOptionCustomDismissAction];
+        
+        // Create the custom actions for expired timer notifications.
+        UNNotificationAction* snoozeAction = [UNNotificationAction
+                                              actionWithIdentifier:@"SNOOZE_ACTION"
+                                              title:@"Snooze"
+                                              options:UNNotificationActionOptionAuthenticationRequired];
+        
+        UNNotificationAction* stopAction = [UNNotificationAction
+                                            actionWithIdentifier:@"STOP_ACTION"
+                                            title:@"Stop"
+                                            options:UNNotificationActionOptionDestructive];
+        UNNotificationAction* forAction = [UNNotificationAction
+                                            actionWithIdentifier:@"FOR_ACTION"
+                                            title:@"forAction"
+                                            options:UNNotificationActionOptionForeground];
+        
+        // Create the category with the custom actions.
+        UNNotificationCategory* expiredCategory = [UNNotificationCategory
+                                                   categoryWithIdentifier:@"TIMER_EXPIRED"
+                                                   actions:@[snoozeAction, stopAction,forAction]
+                                                   intentIdentifiers:@[]
+                                                   options:UNNotificationCategoryOptionNone];
+        
+        // Register the notification categories.
+        UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+        [center setDelegate:self];
+        [center setNotificationCategories:[NSSet setWithObjects:generalCategory, expiredCategory,
+                                           nil]];
+        
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    }
+    else {
+//        if ([[UIDevice currentDevice].systemVersion doubleValue] >= 8.0) {
+//            [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
+//        [[UIApplication sharedApplication] registerForRemoteNotifications];
+
+//        }
+//        else {//(iOS version < iOS8)
+//            [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeBadge)];
+//        }
+    }
+
+}
+
+
 
 
 #pragma mark - UISceneSession lifecycle
@@ -34,6 +95,54 @@
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+#pragma mark - User NotificationCenter Delegate
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
+    NSLog(@"will present : %s", __func__);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)(void))completionHandler{
+    
+    NSLog(@"did receive : %s", __func__);
+    NSLog(@"notification = %@",response.notification.request.content);
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+
+    NSString *toke = [self getHexStringForData:deviceToken];
+    NSLog(@"regisger success : %@",toke);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"regisger failed : %@",error.localizedDescription);
+}
+
+
+- (NSString *)getHexStringForData:(NSData *)data
+{
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 13) {
+        
+        if (![data isKindOfClass:[NSData class]]) {
+            return @"";
+        }
+        NSUInteger len = [data length];
+        char *chars = (char *)[data bytes];
+        NSMutableString *hexString = [[NSMutableString alloc]init];
+        for (NSUInteger i=0; i<len; i++) {
+            [hexString appendString:[NSString stringWithFormat:@"%0.2hhx" , chars[i]]];
+        }
+        return hexString;
+    } else {
+         NSString *token = [[data description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+        token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+        return token;
+    }
 }
 
 
---
+++ PushDemo/pushme.php
@@ -0,0 +1,67 @@
+
+<?php
+
+// Put your device token here (without spaces):
+$deviceToken = '4d2ff9b253e64842510534fe9a512af8955e04f659493006bd3a8cdcd75b0c64';
+
+// Put your private key's passphrase here:密语
+$passphrase = '123456';
+
+// Put your alert message here:
+$title = '喝茶';
+$subtitle = '10点去喝茶';
+$body = '10点喝什么茶，绿茶？红茶？还是青茶?';
+$alert = array('title' => $title,
+			'subtitle' => $subtitle, 
+			'body'	   => $body
+			);
+
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
+	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+	exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$message['aps'] = array(
+	'alert' => $alert,
+	'sound' => 'default',
+	'badge' => 1,
+	'content-available' => 1,	//静默标识
+	'mutable-content' => 1 //富文本标识
+	);
+
+$message['custom_content'] = [
+	'name' => 'bien',
+	'age'  => 30
+];
+
+
+// Encode the payload as JSON
+$payload = json_encode($message);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) .$payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+	echo 'Message not delivered' . PHP_EOL;
+else
+	echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
+    
+?>
